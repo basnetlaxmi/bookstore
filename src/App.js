5de0b1396e685e1296eec1d4b8682c6f29d3ec55
Laxmiba7 <oluxmee@gmail.com>
2022-03-26 14:10:48 +0545

    Add book data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Routes,Route } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import Menu from './components/Menu'
+
 function App() {
-	return (
-		null
-	);
+  return (
+    <div className="App">
+      <Menu />
+         <Routes>
+			 <Route path='/' element={<Books />}/>
+			 <Route path='categories/' element={<Categories />}/>
+          
+		  </Routes>
+       
+      
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Books.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Book from './Book';
+import BookForm from './BookForm';
+
+const Books = (props = { books: [] }) => {
+  let { books } = props;
+  if (!books) books = [];
+  return (
+    <>
+      <ul className="listContainer">
+        {books.map((book) => (
+          <Book key={book.id} book={book} />
+        ))}
+      </ul>
+      <BookForm />
+    </>
+  );
+};
+
+export default Books;
