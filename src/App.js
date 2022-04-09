822f73b0e2fb92f508a225377c82d6ae44c080fe
Laxmiba7 <oluxmee@gmail.com>
2022-04-09 14:40:34 +0545

    Add remove, add funtion with redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Books from './components/Books';
 import Categories from './components/Categories';
 import Menu from './components/Menu';
+import store from './redux/configureStore';
 
 function App() {
   return (
     <div className="App">
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="categories/" element={<Categories />} />
+      <Provider store={store}>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="categories/" element={<Categories />} />
 
-      </Routes>
-
+        </Routes>
+      </Provider>
     </div>
   );
 }
